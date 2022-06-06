00bdd0d201e8afd426f22069fd88b44d4bd4665e
Aulia Achmad <aulachmad@gmail.com>
2022-06-06 16:28:23 +0700

    added file handler and test files

--- fileHandler.py
+++ fileHandler.py
@@ -1,4 +1,16 @@
-fHandler = open("testfile.txt")
+fHandPrompt = input("What is the name of the file that you want to open?\n>>>")
+lookForPrompt = input("What are you looking for?\n>>>")
 
+try :
+    fHandler = open(fHandPrompt)
+except :
+    print("Sorry, we can't find the file that you are looking for. Please try again.")
+    quit()
+    
+counter = 0
 for line in fHandler :
-    print(line)
+    line = line.rstrip()
+    if lookForPrompt in line.lower() :
+        counter = counter + 1
+        print(line)
+print("There are", counter, "lines of", lookForPrompt, "in", fHandPrompt)
